7189583643e3bd47c6d74fd09ab641c771d3f1f0
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-08-13 18:56:32 -0500

    testing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 // Requiring our models and passport as we've configured it
-const db = require('../models');
+const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
