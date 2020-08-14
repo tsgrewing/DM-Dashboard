c69970d738afb16782b6f8a9db50347250aa9f4a
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-08-13 22:06:55 -0500

    fixing linting errors

--- server.js
+++ server.js
@@ -35,17 +35,19 @@ db.sequelize.sync().then(() => {
   });
 });
 
-
 //Loads the handlebars module
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 //Sets our app to use the handlebars engine
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 //Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
-layoutsDir: __dirname + '/views/layouts',
-}));
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-res.render('main', {layout : 'index'});
+app.engine(
+  "handlebars",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts"
+  })
+);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render("main", { layout: "index" });
 });
