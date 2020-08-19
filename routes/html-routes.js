c1258810140fea5343bebb545ba94389f3211647
Billy Hicks <billy_jerome_jr@me.com>
2020-08-18 21:01:57 -0500

    fixed last pushs mistakes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,14 +16,14 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.render("index");
+      res.render("login");
     }
-    res.render("members");
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("members");
+    res.render("createCharacter");
   });
 };
