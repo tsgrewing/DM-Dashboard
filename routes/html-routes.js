6d62e22f7ce52875cf9cdd842eccf6f51e7d5ae9
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-08-20 15:02:50 -0500

    setting up divs for manuals and map

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,10 @@
 const db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const character = require("../models/character");
+// const character = require("../models/character");
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
@@ -15,8 +18,9 @@ module.exports = function(app) {
   });
 
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the character create page
     if (req.user) {
+      res.render("createCharacter");
     }
     res.render("login");
   });
@@ -28,17 +32,51 @@ module.exports = function(app) {
   });
 
   app.get("/index", isAuthenticated, (req, res) => {
+    let hbsObject = {};
+    // get character info from database
     db.Character.findAll().then(data => {
-      // console.log(data);
-      const hbsObject = {
+      const charObj = {
         characters: data
       };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+      hbsObject = { ...charObj };
     });
+      // then get spell info from dNd api and add it to the object passed to handlebars
+      $.ajax({
+        url: "https://www.dnd5eapi.co/api/spells",
+        method: "GET"
+    }).then(spellRes => {
+      console.log(spellRes);
+        const spellObj = {
+          spells: spellRes.results
+        };
+        hbsObject = { ...hbsObject, ...spellObj}
+        console.log(spellObj)
+      });
 
-  app.get("/index", isAuthenticated, (req, res) => {
-    res.render("index");
+      //  get monster info from api and add that to hbsObject
+    $.ajax({
+        url: "https://www.dnd5eapi.co/api/monsters",
+        method: "GET"
+    }).then(monsterRes => {
+        const monsterObj = {
+          monsters: monsterRes
+        };
+        hbsObject = { ...hbsObject, ...monsterObj}
+      });
+        // then get equipment info and add that to the hbs onbject
+
+    $.ajax({
+        url: "https://www.dnd5eapi.co/api/equipment",
+        method: "GET"
+    }).then(equipRes => {
+        const equipObj = {
+          equipment: equipRes.results
+        };
+        hbsObject = { ...hbsObject, ...equipObj}
+    //     // then render the index page
+    // }).then(
+      console.log(hbsObject)
+      res.render("index", hbsObject)
+    });
   });
-});
-}
+};
