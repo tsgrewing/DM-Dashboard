569ac4f94af1cb5ea3f28a80d234ed4f9898efa9
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-04-06 10:44:58 -0500

    shortening response to res for equipment, monster and spell responses

--- public/js/home.js
+++ public/js/home.js
@@ -36,14 +36,14 @@ $(document).ready(() => {
     $.ajax({
       url: `https://www.dnd5eapi.co${url}`,
       method: "GET"
-    }).then(response => {
+    }).then(res => {
       spellDiv.html(
-        `<tr><th> Description: </th> <td> ${response.desc[0]}</td></tr>
-        <tr><th> Range: </th> <td> ${response.range}</td></tr>
-        <tr><th> Level: </th> <td> ${response.level}</td></tr>
-        <tr><th> Casting Time: </th> <td> ${response.casting_time}</td></tr>
-        <tr><th> Duration: </th> <td> ${response.duration}</td></tr>
-        <tr><th> School: </th> <td> ${response.school.name}</td></tr>
+        `<tr><th> Description: </th> <td> ${res.desc[0]}</td></tr>
+        <tr><th> Range: </th> <td> ${res.range}</td></tr>
+        <tr><th> Level: </th> <td> ${res.level}</td></tr>
+        <tr><th> Casting Time: </th> <td> ${res.casting_time}</td></tr>
+        <tr><th> Duration: </th> <td> ${res.duration}</td></tr>
+        <tr><th> School: </th> <td> ${res.school.name}</td></tr>
         `
       );
     });
@@ -53,20 +53,20 @@ $(document).ready(() => {
     $.ajax({
       url: `https://www.dnd5eapi.co${url}`,
       method: "GET"
-    }).then(response => {
+    }).then(res => {
       monsterDiv.html(
-        `<tr><th> Size: </th> <td> ${response.size}</td></tr>
-      <tr><th> Type: </th> <td> ${response.type}</td></tr>
-      <tr><th> Alignment: </th> <td> ${response.alignment}</td></tr>
-      <tr><th> Armor Class: </th> <td> ${response.armor_class}</td></tr>
-      <tr><th> Hit Points: </th> <td> ${response.hit_points}</td></tr>
-      <tr><th> Hit Dice: </th> <td> ${response.hit_dice}</td></tr>
-      <tr><th> Strength: </th> <td> ${response.strength}</td></tr>
-      <tr><th> Dexterity: </th> <td> ${response.dexterity}</td></tr>
-      <tr><th> Constitution: </th> <td> ${response.constitution}</td></tr>
-      <tr><th> Intelligence: </th> <td> ${response.intelligence}</td></tr>
-      <tr><th> Wisdom: </th> <td> ${response.wisdom}</td></tr>
-      <tr><th> Charisma: </th> <td> ${response.charisma}</td></tr>
+        `<tr><th> Size: </th> <td> ${res.size}</td></tr>
+      <tr><th> Type: </th> <td> ${res.type}</td></tr>
+      <tr><th> Alignment: </th> <td> ${res.alignment}</td></tr>
+      <tr><th> Armor Class: </th> <td> ${res.armor_class}</td></tr>
+      <tr><th> Hit Points: </th> <td> ${res.hit_points}</td></tr>
+      <tr><th> Hit Dice: </th> <td> ${res.hit_dice}</td></tr>
+      <tr><th> Strength: </th> <td> ${res.strength}</td></tr>
+      <tr><th> Dexterity: </th> <td> ${res.dexterity}</td></tr>
+      <tr><th> Constitution: </th> <td> ${res.constitution}</td></tr>
+      <tr><th> Intelligence: </th> <td> ${res.intelligence}</td></tr>
+      <tr><th> Wisdom: </th> <td> ${res.wisdom}</td></tr>
+      <tr><th> Charisma: </th> <td> ${res.charisma}</td></tr>
       `
       );
     });
@@ -76,10 +76,10 @@ $(document).ready(() => {
     $.ajax({
       url: `https://www.dnd5eapi.co${url}`,
       method: "GET"
-    }).then(response => {
+    }).then(res => {
       equipDiv.html(
-        `<tr><th> Cost: </th> <td> ${response.cost.quantity} ${response.cost.unit}</td></tr>
-      <tr><th> Weight: </th> <td> ${response.weight} lbs</td></tr>
+        `<tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
+      <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
       `
       );
     });
