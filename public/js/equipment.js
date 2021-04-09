86e0fc139fca01c81d746fccaa514a3e1c958c92
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-04-09 13:17:29 -0500

    creating case for equipment packs

--- public/js/equipment.js
+++ public/js/equipment.js
@@ -8,312 +8,133 @@ const updateEquipment = url => {
     method: "GET"
   }).then(res => {
     console.log(res);
+    if (res.desc) {
+      equipDiv.html(
+        `<tr><th> Category: </th> <td> ${res.equipment_category.name}
+            <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
+            <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
+            <tr><th> Description: </th> <td> ${res.desc} </td></tr>
+            `
+      );
+    } else {
+      equipDiv.html(
+        `<tr><th> Category: </th> <td> ${res.equipment_category.name}
+      <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
+      <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
+      `
+      );
+    }
     // determine what type of equipment it is and display info accordingly
     switch (res.equipment_category.index) {
       case "adventuring-gear":
-        if (res.desc) {
-          equipDiv.html(
-            `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-                <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-                <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-                <tr><th> Description: </th> <td> ${res.desc} </td></tr>
-                `
-          );
-        } else if (res.gear_category === "ammunition") {
-          equipDiv.html(
-            `<tr><th> Category: </th> <td> ${res.gear_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-          );
-        } else {
-          equipDiv.html(
-            `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-          );
-        }
-        break;
-      case "ammunition":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
-        break;
-      case "arcane-foci":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+        if (res.gear_category.index){
+          switch(res.gear_category.index) {
+            case "equipment-packs":
+              equipDiv.append(`
+              <tr><th> Contents: </th></tr>
+              `);
+              (res.contents).forEach(elem => {
+                equipDiv.append(`
+                <tr><td>${elem.item.name}</td><td> x ${elem.quantity}`)
+              });
+              break;
+          }
+        } else if ()
         break;
       case "armor":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
-        break;
-      case "artisans-tools":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
-        break;
-      case "druidic-foci":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+        equipDiv.append(`
+        
+        `);
         break;
       case "equipment-packs":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "gaming-sets":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "heavy-armor":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "holy-symbols":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "kits":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "land-vehicles":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "light-armor":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "matrial-melee-weapons":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "martial-ranged-weapons":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "medium-armor":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "mounts-and-other-animals":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "mounts-and-vehicles":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "musical-intstruments":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "other-tools":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "potion":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "ring":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "rod":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "scroll":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "shields":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "simple-melee-weapons":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "simple-ranged-weapons":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "simple-weapons":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "staff":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "standard-gear":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "tack-harness-and-drawn-vehicles":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "tools":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "wand":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "waterborne-vehicles":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "weapon":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       case "wondrous-items":
-        equipDiv.html(
-          `<tr><th> Category: </th> <td> ${res.equipment_category.name}
-          <tr><th> Cost: </th> <td> ${res.cost.quantity} ${res.cost.unit}</td></tr>
-          <tr><th> Weight: </th> <td> ${res.weight} lbs</td></tr>
-          `
-        );
+
         break;
       default:
         equipDiv.html(
